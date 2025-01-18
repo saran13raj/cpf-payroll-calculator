17f565d7d355b1bd002257bff084eafc4a13a25a
saran13raj <saran13raj@gmail.com>
2025-01-18 15:46:09 +0530

    add env sample

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,24 @@
 import { Logger, Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CPFModule } from './cpf/cpf.module';
 import { EmployeeModule } from './employee/employee.module';
 
-const dbURL = 'mongodb://localhost:27017';
+const DATABASE_NAME = process.env.DATABASE_NAME ?? 'cpf-local';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(dbURL, {
-      dbName: 'cpf-local',
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.DATABASE_URL, {
+      dbName: DATABASE_NAME,
       connectionFactory: (connection) => {
         connection.on('connected', () => {
-          Logger.log('MongoDB is connected');
+          Logger.log('MongoDB is connected to db >> ' + DATABASE_NAME);
         });
         connection._events.connected();
         return connection;
--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,14 @@ import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './http-exception.filter';
 
 async function bootstrap() {
+  const PORT = process.env.PORT ?? 3000;
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'debug', 'log', 'warn'],
   });
-  Logger.log('app running on port 3000');
 
   app.useGlobalFilters(new HttpExceptionFilter());
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(PORT);
+  Logger.log(`app running on port >> ${PORT}`);
 }
 bootstrap();
